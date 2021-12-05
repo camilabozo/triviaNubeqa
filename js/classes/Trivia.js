a1af0cd9d112482b21eb5142b8120bbc43b97552
leandromanciagli <leandromanciagli@gmail.com>
2021-12-05 03:16:22 -0300

    Se añaden estilos segun estados de botones

--- js/classes/Trivia.js
+++ js/classes/Trivia.js
@@ -29,13 +29,31 @@ class Trivia
         const finishFunction = () => {
             this.finish()
         }
-        finishButton.style.backgroundColor = "#46B7E8";
+        finishButton.style.backgroundColor = "var(--lightBlue)";
+        finishButton.style.color = "var(--intenseBlue)";
         finishButton.style.transform = "scale(1)";
         finishButton.style.animation = "pulseA 2s infinite";
         finishButton.style.cursor = "pointer";
         finishButton.addEventListener('click', finishFunction);
         finishButton.disabled = false;
+        document.styleSheets[0].insertRule('.blue_button:hover { background-color: var(--intermediateBlue); color: white; }', 23);
+        // document.styleSheets[0].cssRules[0].style.backgroundColor= 'red';
+        console.log(document.styleSheets[0]);
+        
     }
+
+    // changeButtonHoverStyle()
+    // {
+    //     var css = '.blue_button:hover { background-color: var(--intermediateBlue); color: white; }';
+    //     var style = document.createElement('style');
+    //     style.type = 'text/css';
+    //     if (style.styleSheet) {
+    //         style.styleSheet.cssText = css;
+    //     } else {
+    //         style.appendChild(document.createTextNode(css));
+    //     }
+    //     document.getElementsByTagName('head')[0].appendChild(style);
+    // }
 
     finish()
     {
